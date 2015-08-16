a150072b1943636504f8bddf8f9a7cccf5288616
Sahil Thakur <sahilgaucho@gmail.com>
2015-08-16 15:39:59 +0530

    Update MostUsedAppReducer.java

--- MostUsedApp/src/MostUsedAppReducer.java
+++ MostUsedApp/src/MostUsedAppReducer.java
@@ -4,8 +4,10 @@ import java.util.Iterator;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 
-public class MostUsedAppReducer extends MapReduceBase implements Reducer<Text, DoubleWritable, Text, DoubleWritable> {
-	public void reduce(Text key, Iterator<DoubleWritable> values, OutputCollector<Text, DoubleWritable> output, Reporter reporter) throws IOException{
+public class MostUsedAppReducer extends MapReduceBase implements
+Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+	public void reduce(Text key, Iterator<DoubleWritable> values, OutputCollector<Text, DoubleWritable> output,
+	Reporter reporter) throws IOException{
 		double sum = 0;
 		while(values.hasNext()){
 			sum += values.next().get();
